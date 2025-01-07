8f6a3e6976d55c63cdd5cfe483ff862cfc5c19c5
Rakshit Mudgal <rakshitmudgal@gmail.com>
2025-01-08 01:18:17 +0530

    first commit

---
+++ client/src/Constants/CelebrityData.js
@@ -0,0 +1,14 @@
+import apj from "../assets/images/apj.png";
+import billGates from "../assets/images/billGates.png";
+import einstein from "../assets/images/einstein.png";
+import nelsonMandela from "../assets/images/nelsonMandela.png";
+import steveJobs from "../assets/images/steveJobs.png";
+
+export const celebrities = [
+    {
+        title: "RamKrishna Sameriya",
+        description: "Ramkrishna Sameriya is the founder and CEO of Pioneer Udyam Academy LLP, a social venture supporting rural entrepreneurs in India. He has over 15 years of experience in branding, marketing, and business development for various organizations. He is also an ICF-certified purpose coach, author, traveler, and TEDx speaker.",
+        image: nelsonMandela, 
+    },
+
+]
---
+++ client/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
+        'nunito-sans': ['Nunito Sans', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+      animation: {
+        'bg-animate': 'backgroundMove 5s infinite linear',
+      },
+      keyframes: {
+        backgroundMove: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [require('daisyui')],
+};
