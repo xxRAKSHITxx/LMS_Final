8060ee4963eb941a560060fdb405ff28458d1620
Rakshit Mudgal <rakshitmudgal@gmail.com>
2025-01-15 23:20:34 +0530

    My changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ||5000;
 import {v2 as cloudinary} from 'cloudinary';
 import Razorpay from "razorpay"; 
          
