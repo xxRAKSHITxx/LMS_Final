8649097ea70201410d655f3d34951520284d3bcf
Rakshit Mudgal <rakshitmudgal@gmail.com>
2025-01-17 03:10:08 +0530

    d

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -24,18 +24,10 @@ const authorisedRoles = (...roles) => async (req, res, next) => {
     next();
 }
 
-// const authorizeSubscriber = async (req, res, next) => {
-//     const {role, id} = req.user; 
-//     const user = await userModel.findById(id);
-//     const subscriptionStatus = user.subscription.status;
-//     if (role !== 'ADMIN' && subscriptionStatus !== 'active') {
-//         return next(
-//             new AppError('Please subscribce to access this route!', 403)
-//         )
-//     }
-
-//     next();
-// }
+// Optional: If you want to keep the function but make it a pass-through
+const authorizeSubscriber = async (req, res, next) => {
+    next();
+}
 
 export {
     isLoggedIn,
